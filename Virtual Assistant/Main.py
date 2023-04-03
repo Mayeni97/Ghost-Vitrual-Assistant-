935a3454cfa02ac8ae8969018039fb2b1e2b5232
Mayowa Ayeni <Mayeni97@gmail.com>
2023-04-02 21:54:40 -0400

    Created Vitual Assitant, and making weather api

---
+++ Virtual Assistant/Main.py
@@ -0,0 +1,74 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine =  pyttsx3.init()
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+engine.setProperty('rate', 150)     # setting up new voice rate
+voices = engine.getProperty('voices') 
+engine.setProperty('voice', voices[0].id)    #changing index, changes voices. 0 for male 
+#engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female 
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    command = ''
+    try:
+        with sr.Microphone() as source:
+            print('Listening ...')
+            listener.adjust_for_ambient_noise(source)
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'Ghost' in command:
+                command = command.replace('Ghost', '')
+    except:
+        pass
+
+    return command
+
+def run_assistant():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 2)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        date = datetime.datetime.now().strftime("%B %d, %Y")
+        print(date)
+        talk("Today is" + date)
+    elif "Do you want to go on a date with me?" in command:
+        talk("Arent you dating someone?, plus i'm dating WIFI at the moment")
+    elif "joke" in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Say that again, please')
+    
+ 
+engine.say("Hello, I'm Ghost, your virtual assistant.")
+engine.say("How can I help you?")
+engine.runAndWait()
+
+while True:
+    run_assistant()
+
+        
+
+
---
+++ Virtual Assistant/weather.py
@@ -0,0 +1,44 @@
+from pyowm import OWM
+from geopy import Nominatim
+import datetime
+import weatherstack
+
+class Weather:
+    # location of the weather
+    __location = "Springfield, US"
+
+    # API Key
+    api_key = "6b0a6f6a62dff7712f2c44394f17270b"
+
+    def __init__(self):
+        self.ws = weatherstack.Client(self.api_key)
+        locator = Nominatim(user_agent="geo")
+        city = "Springfield"
+        country = "United States"
+        self.__location = city + "," + country
+        loc = locator.geocode(self.__location)
+        self.lat = loc.latitude
+        self.long = loc.longitude
+
+    @property
+    def weather(self):
+        today = datetime.date.today()
+        tomorrow = today + datetime.timedelta(days=1)
+        historical_today = self.ws.historical(
+            lat=self.lat,
+            lon=self.long,
+            start_date=today,
+            end_date=today,
+            hourly=True
+        )
+        historical_tomorrow = self.ws.historical(
+            lat=self.lat,
+            lon=self.long,
+            start_date=tomorrow,
+            end_date=tomorrow,
+            hourly=True
+        )
+        return historical_today, historical_tomorrow
+
+my_weather = Weather()
+print(my_weather.weather)
